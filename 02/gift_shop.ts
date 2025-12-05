137e1df1ecb55cf334f5423b4f07965c3b059923
Marius Meier <marius.meier24@gmail.com>
2025-12-05 13:20:57 +0200

    code cleanup, change of let to const for substrings

--- 02/gift_shop.ts
+++ 02/gift_shop.ts
@@ -9,13 +9,12 @@ fs.readFile('input.txt', (err: NodeJS.ErrnoException | null, file: Buffer) => {
   
   lines.forEach((line: string): void => {
     const ids: string[] = line.split('-')
-    const rangeSize = Number(ids[1]) - Number(ids[0])
     let currId: number = Number(ids[0])
     const lastId: number = Number(ids[1])
     while (currId <= lastId) {
      if (currId.toString().length % 2 === 0) {
-      let subOne: string = currId.toString().substring(0, currId.toString().length / 2)
-      let subTwo: string = currId.toString().substring(currId.toString().length / 2)
+      const subOne: string = currId.toString().substring(0, currId.toString().length / 2)
+      const subTwo: string = currId.toString().substring(currId.toString().length / 2)
       if (subOne === subTwo) {
         finalResult += Number(currId)
       }
